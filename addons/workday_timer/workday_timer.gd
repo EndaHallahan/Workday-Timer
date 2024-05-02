3c23b5efc1581645a1e46793d6d557775bfe9ed1
EndaHallahan <endah@udel.edu>
2024-05-02 08:08:58 -0400

    Prepared for submission to Asset Library.

--- addons/workday_timer/workday_timer.gd
+++ addons/workday_timer/workday_timer.gd
@@ -1,14 +1,16 @@
 @tool
 extends EditorPlugin
 
-var timer_scene = preload("res://addons/workday_timer/workday_timer.tscn")
-var timer
-var container = 0
+const TimerScene := preload("res://addons/workday_timer/workday_timer.tscn")
+var timer: Node
+var container := 0
+
 
 func _enter_tree():
-	timer = timer_scene.instantiate()
+	timer = TimerScene.instantiate()
 	timer.name = "addon_countdown"
 	add_control_to_container(container, timer)
+	
 	
 func _exit_tree():
 	remove_control_from_container(container, timer)
